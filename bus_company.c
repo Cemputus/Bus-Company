fedc0bc449a69eaf7218c78c323e94f7094557da
Emmanuel Nsubuga <107301618+Cemputus@users.noreply.github.com>
2023-07-11 02:09:52 +0300

    bus_company.c

--- bus_company.c
+++ bus_company.c
@@ -1,5 +1,5 @@
 #include <stdio.h>            
-#include <string.h>                                     
+#include <string.h>                                      
       
 int login();          
 int main(){                       
